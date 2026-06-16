32d1d6fab0cfc1f3af9453fa032becad4ffb8e69
Dennis <buivuthanhduy@gmail.com>
2026-06-16 17:54:39 +0700

    Feat: Update docker

--- scripts/deploy-update.sh
+++ scripts/deploy-update.sh
@@ -94,8 +94,8 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
-    location /umami/ {
-        proxy_pass http://127.0.0.1:3001/;
+    location /umami {
+        proxy_pass http://127.0.0.1:3001;
         proxy_http_version 1.1;
         proxy_set_header Host              \$host;
         proxy_set_header X-Real-IP         \$remote_addr;
